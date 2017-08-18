04b6d2bbda79801334bad6689cb8c74a1f7a3b3e
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2017-08-18 15:12:57 +0200

    add bidirectional communication (working for js to native and back) #7

--- WebViewController.swift
+++ WebViewController.swift
@@ -13,8 +13,7 @@ import WebKit
 class WebViewController: UIViewController, WKScriptMessageHandler {
     
     @IBOutlet var containerView: UIView!
-    var webView: WKWebView?
-    
+    var webView: WKWebView!
     
     override func viewDidLoad()
     {
@@ -32,10 +31,16 @@ class WebViewController: UIViewController, WKScriptMessageHandler {
             let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
             let baseUrl = URL(fileURLWithPath: filePath)
             self.webView!.loadHTMLString(contents as String, baseURL: baseUrl)
+            
+            // TODO not working at this time
+            let exec2 = "sayHello()"
+            webView.evaluateJavaScript(exec2, completionHandler: nil)
         }
         catch {
             print ("File HTML error")
         }
+        
+        
     }
     
     
@@ -48,7 +53,8 @@ class WebViewController: UIViewController, WKScriptMessageHandler {
         super.loadView()
         
         let contentController = WKUserContentController()
-        
+        let config = WKWebViewConfiguration()
+        /*
         let userScript = WKUserScript(
             source: "redHeader()",
             injectionTime: WKUserScriptInjectionTime.atDocumentEnd,
@@ -56,24 +62,40 @@ class WebViewController: UIViewController, WKScriptMessageHandler {
         )
         
         contentController.addUserScript(userScript)
+        */
         contentController.add(
             self,
-            name: "callbackHandler"
+            name: "observe"
         )
+ 
         
-        let config = WKWebViewConfiguration()
         config.userContentController = contentController
         
         self.webView = WKWebView(
             frame: self.containerView.bounds,
             configuration: config
         )
+        
         self.view = self.webView
     }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("Received event \(message.body)")
+        
+     /*
         if(message.name == "callbackHandler") {
             print("JavaScript is sending a message \(message.body)")
         }
+       */
+        print(message.body)
+        
+        
+        let device = UIDevice()
+        let deviceValue = device.value(forKey: message.body as! String)
+        let mystring = String(describing: deviceValue)
+        
+        
+        let exec = "set_headline(\"\(mystring)\")"
+        webView.evaluateJavaScript(exec, completionHandler: nil)
     }
 }
--- www/main.js
+++ www/main.js
@@ -1,15 +1,16 @@
-function callNativeApp () {
-    try {
-        webkit.messageHandlers.callbackHandler.postMessage("Hello from JavaScript");
-    } catch(err) {
-        console.log('The native context does not exist yet');
-    }
+var headline = $("#headline");
+var selection = $("#selector");
+
+function set_headline (text) {
+    headline.text(text);
 }
 
-setTimeout(function () {
-           callNativeApp();
-           }, 5000);
-
-function redHeader() {
-    document.querySelector('h1').style.color = "red";
+function call_native () {
+    var prop = selection.val();
+    set_headline("asked for " + prop + "...");
+    window.webkit.messageHandlers.observe.postMessage(prop);
 }
+
+setTimeout(call_native, 1000);
+
+selection.on("change", call_native);
